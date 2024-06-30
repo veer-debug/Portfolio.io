69f70e16161a98692120bc41b7cb14f2eae8429a
Ranveer Kumar <122090985+veer-debug@users.noreply.github.com>
2024-06-30 21:48:53 +0530

    Update projext_element.jsx

--- src/Pages/projext_element.jsx
+++ src/Pages/projext_element.jsx
@@ -26,7 +26,7 @@ export const Projectelement = () => {
       imge: "https://github.com/veer-debug/music/blob/main/Realstate.png?raw=true",
       image: "https://cdn-icons-png.flaticon.com/128/1083/1083968.png",
       name: "Match My Mansion",
-      description: " Match My Mansion is a machine learning-powered platform offering accurate price predictions and personalized property recommendations based on price, location, and type, revolutionizing the real estate search process.",
+      description: " Match My Mansion is a web-based platform that revolutionizes property search using advanced machine learning. It offers accurate price predictions and personalized property recommendations based on factors like price, location, and type. The platform simplifies the real estate search process, making it efficient and user-centric.",
       tool: ["Python" , "Flask",  "CSS" , "HTML" , "Sk-Learn" , "Numpy" , "Pandas" ],
       netlify: "https://github.com/veer-debug/MatchMyMansion.git",
       github: "https://github.com/veer-debug/MatchMyMansion.git"
